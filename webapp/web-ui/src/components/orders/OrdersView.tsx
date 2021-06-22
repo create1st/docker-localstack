c5bc85d9db88268732b22deac06d81feadcd60a1
Sebastian Gil <sebastian.gil@gmail.com>
2021-06-22 23:33:48 +1000

    JS hooks + Material-UI (#93)
    * Convert to React hooks
    
    * Convert to Axios
    
    * Material-ui

--- webapp/web-ui/src/components/orders/OrdersView.tsx
+++ webapp/web-ui/src/components/orders/OrdersView.tsx
@@ -1,52 +1,54 @@
-import React from 'react';
-import { WithAuth0Props, withAuth0 } from '@auth0/auth0-react';
-import { GetAccessTokenSilently } from '../../common/auth/auth0';
-import { Orders } from '../../model/Order';
+import React, {useEffect, useState} from 'react';
+import {useAuth0} from '@auth0/auth0-react';
+import {GetAccessTokenSilently} from '../../common/auth/auth0';
+import {Orders} from '../../model/Order';
 import OrdersTable from "./OrdersTable";
+import axios, {AxiosResponse} from "axios";
 
 const ordersUrl = `http://localhost:8080/orders`;
 
-interface OrderViewState {
-  orders: Orders
+interface OrdersViewState {
+    orders: Orders
 }
 
-class OrdersView extends React.Component<WithAuth0Props, OrderViewState> {
-  constructor(props: WithAuth0Props) {
-    super(props);
-    this.state = { orders: [] };
-  }
+type SetOrdersViewState = (value: (((prevState: OrdersViewState) => OrdersViewState) | OrdersViewState)) => void;
 
-  componentDidMount() {
-    const {
-      getAccessTokenSilently,
-    } = this.props.auth0;
-    this.getOrders(getAccessTokenSilently);
-  }
-
-  async getOrders(getAccessTokenSilently: GetAccessTokenSilently) {
-    try {
-      const accessToken = await getAccessTokenSilently();
-      const ordersResponse = await fetch(ordersUrl, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-      const orders = await ordersResponse.json();
-      this.setState({
-        orders: orders
-      });
-    } catch (e: any) {
-      console.log(e.message);
-    }
-  }
-
-  render() {
+// https://auth0.github.io/auth0-react/
+const OrdersView = () => {
+    const {getAccessTokenSilently} = useAuth0();
+    const [ordersViewState, setOrdersViewState] = useState<OrdersViewState>({
+        orders: []
+    });
+    OrdersViewStateUpdater(getAccessTokenSilently, setOrdersViewState);
     return (
-      <div>
-        <OrdersTable orders={this.state.orders} />
-      </div>
+        <div>
+            <OrdersTable orders={ordersViewState.orders}/>
+        </div>
     )
-  }
 }
 
-export default withAuth0(OrdersView);
+const OrdersViewStateUpdater = (getAccessTokenSilently: GetAccessTokenSilently, setOrdersViewState: SetOrdersViewState) => useEffect(() => {
+    getAccessTokenSilently()
+        .then(fetchOrders)
+        .then(response => response.data)
+        .then(toOrderViewState)
+        .then(setOrdersViewState)
+        .catch(errorHandler);
+}, [getAccessTokenSilently, setOrdersViewState])
+
+const fetchOrders = (accessToken: String): Promise<AxiosResponse<Orders>> =>
+    axios.get(ordersUrl, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+
+const toOrderViewState = (resp: Orders): OrdersViewState => ({
+    orders: resp
+})
+
+const errorHandler = (error: any) => {
+    console.error("Failed to get orders" + error);
+}
+
+export default OrdersView;
--- webapp/web-ui/src/components/webapp/Webapp.tsx
+++ webapp/web-ui/src/components/webapp/Webapp.tsx
@@ -1,47 +1,88 @@
-import { withAuth0, WithAuth0Props } from '@auth0/auth0-react';
-import { User } from '@auth0/auth0-spa-js';
+import {useAuth0} from '@auth0/auth0-react';
+import {User} from '@auth0/auth0-spa-js';
 import React from 'react';
-import { LoginWithRedirect, Logout } from '../../common/auth/auth0';
+import {LoginWithRedirect, Logout} from '../../common/auth/auth0';
 import OrdersView from '../orders/OrdersView';
+import {
+    AppBar,
+    Button,
+    CircularProgress,
+    IconButton,
+    makeStyles,
+    Paper,
+    Theme,
+    Toolbar,
+    Typography
+} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import {ClassNameMap} from "@material-ui/core/styles/withStyles";
 
-// For Auth0 hooks see example on
-// https://github.com/auth0/auth0-react#use-with-a-class-component
-class Webapp extends React.Component<WithAuth0Props> {
-  render() {
+const Webapp = () => {
     const {
-      isAuthenticated,
-      isLoading,
-      loginWithRedirect,
-      user,
-      logout,
-    } = this.props.auth0;
+        isAuthenticated,
+        isLoading,
+        loginWithRedirect,
+        user,
+        logout,
+    } = useAuth0();
+    const classes = useStyles();
 
-    if (isLoading) return this.renderLoading()
-    if (!isAuthenticated) return this.renderLogin(loginWithRedirect)
-    return this.renderComponent(user!, logout)
-  }
-
-  renderLoading() {
-    return (
-      <div>Loading...</div>
-    )
-  }
-
-  renderLogin(loginWithRedirect: LoginWithRedirect) {
-    return (
-      <button onClick={loginWithRedirect}>Log in</button>
-    )
-  }
-
-  renderComponent(user: User, logout: Logout) {
-    return (
-      <div>
-        <OrdersView />
-        <div>Hello {user ? user.name : "guest"}</div>
-        <button onClick={() => logout({ returnTo: window.location.origin })}>Log out</button>
-      </div>
-    )
-  }
+    if (isLoading) return renderLoading(classes)
+    if (!isAuthenticated) return renderLogin(loginWithRedirect, classes)
+    return renderComponent(user!, logout, classes)
 }
 
-export default withAuth0(Webapp);
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+type Styling = ClassNameMap<"root" | "menuButton" | "title">;
+
+const renderLoading = (classes: Styling) => (
+    <div className={classes.root}>
+        <CircularProgress />
+    </div>
+)
+
+const renderLogin = (loginWithRedirect: LoginWithRedirect, classes: Styling) => (
+    <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon/>
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>Please login</Typography>
+                <Button color="inherit"  onClick={loginWithRedirect}>Log in</Button>
+            </Toolbar>
+        </AppBar>
+    </div>
+)
+
+const renderComponent = (user: User, logout: Logout, classes: Styling) => (
+    <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon/>
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    Hello {user ? user.name : "guest"}
+                </Typography>
+                <Button color="inherit" onClick={() => logout({returnTo: window.location.origin})}>Log out</Button>
+            </Toolbar>
+        </AppBar>
+        <Paper>
+            <OrdersView/>
+        </Paper>
+    </div>
+)
+
+export default Webapp;
