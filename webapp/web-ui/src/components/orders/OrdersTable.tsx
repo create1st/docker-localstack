07f466ce550ce8cc93b8014073b8f5e40aabffaa
Sebastian Gil <sebastian.gil@gmail.com>
2021-06-22 23:33:06 +1000

    Material-ui

--- webapp/web-ui/src/components/orders/OrdersTable.tsx
+++ webapp/web-ui/src/components/orders/OrdersTable.tsx
@@ -1,45 +1,81 @@
-import { Order, Orders } from "../../model/Order";
+import {Order, Orders} from "../../model/Order";
+import {
+    makeStyles,
+    Paper,
+    Table, TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Theme,
+    withStyles
+} from "@material-ui/core";
 
-const OrdersTable = ({ orders }: { orders: Orders}) => (
-  <div>
-    <table id="ordersTable">
-      <thead>
-        <tr>
-          <th>User ID</th>
-          <th>Transaction ID</th>
-          <th>Order status</th>
-          <th>Timestamp</th>
-        </tr>
-      </thead>
-      <OrdersTableBody orders={orders}/>      
-    </table>
-  </div>
-)
 
-const OrdersTableBody = ({ orders }: { orders: Orders}) => {
-  let rows = (orders.length > 0)
-    ? orders.map((order,index) => orderRow(order, index))
-    : noResultsRow()
-  return (
-    <tbody>
-      {rows}
-    </tbody>
-  )
+const StyledTableCell = withStyles((theme: Theme) => ({
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme: Theme) => ({
+    root: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.action.hover,
+        },
+    },
+}))(TableRow);
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 700,
+    },
+});
+
+const OrdersTable = ({orders}: { orders: Orders }) => {
+    const classes = useStyles();
+
+    return (
+        <TableContainer component={Paper}>
+            <Table
+                id="ordersTable"
+                className={classes.table}
+                aria-label="customized table"
+            >
+                <TableHead>
+                    <TableRow>
+                    <StyledTableCell>User ID</StyledTableCell>
+                    <StyledTableCell>Transaction ID</StyledTableCell>
+                    <StyledTableCell>Order status</StyledTableCell>
+                    <StyledTableCell>Timestamp</StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <OrdersTableBody orders={orders}/>
+            </Table>
+        </TableContainer>
+    );
+}
+
+const OrdersTableBody = ({orders}: { orders: Orders }) => {
+    let rows = orders.map((order, index) => orderRow(order, index))
+    return (
+        <TableBody>
+        {rows}
+        </TableBody>
+    )
 }
 
 const orderRow = (order: Order, index: number) => (
-  <tr key={index}>
-    <td>{order.userId}</td>
-    <td>{order.transactionId}</td>
-    <td>{order.orderStatus}</td>
-    <td>{order.timestamp}</td>
-  </tr>
-)
-
-const noResultsRow = () => (
-  <tr>
-    <td className="nodata" colSpan={4}>Nothing found to display.</td>
-  </tr>
+    <StyledTableRow key={index}>
+        <StyledTableCell>{order.userId}</StyledTableCell>
+        <StyledTableCell>{order.transactionId}</StyledTableCell>
+        <StyledTableCell>{order.orderStatus}</StyledTableCell>
+        <StyledTableCell>{order.timestamp}</StyledTableCell>
+    </StyledTableRow>
 )
 
 export default OrdersTable;
--- webapp/web-ui/src/components/webapp/Webapp.tsx
+++ webapp/web-ui/src/components/webapp/Webapp.tsx
@@ -3,6 +3,19 @@ import {User} from '@auth0/auth0-spa-js';
 import React from 'react';
 import {LoginWithRedirect, Logout} from '../../common/auth/auth0';
 import OrdersView from '../orders/OrdersView';
+import {
+    AppBar,
+    Button,
+    CircularProgress,
+    IconButton,
+    makeStyles,
+    Paper,
+    Theme,
+    Toolbar,
+    Typography
+} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import {ClassNameMap} from "@material-ui/core/styles/withStyles";
 
 const Webapp = () => {
     const {
@@ -12,25 +25,63 @@ const Webapp = () => {
         user,
         logout,
     } = useAuth0();
+    const classes = useStyles();
 
-    if (isLoading) return renderLoading()
-    if (!isAuthenticated) return renderLogin(loginWithRedirect)
-    return renderComponent(user!, logout)
+    if (isLoading) return renderLoading(classes)
+    if (!isAuthenticated) return renderLogin(loginWithRedirect, classes)
+    return renderComponent(user!, logout, classes)
 }
 
-const renderLoading = () => (
-    <div>Loading...</div>
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+type Styling = ClassNameMap<"root" | "menuButton" | "title">;
+
+const renderLoading = (classes: Styling) => (
+    <div className={classes.root}>
+        <CircularProgress />
+    </div>
 )
 
-const renderLogin = (loginWithRedirect: LoginWithRedirect) => (
-    <button onClick={loginWithRedirect}>Log in</button>
+const renderLogin = (loginWithRedirect: LoginWithRedirect, classes: Styling) => (
+    <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon/>
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>Please login</Typography>
+                <Button color="inherit"  onClick={loginWithRedirect}>Log in</Button>
+            </Toolbar>
+        </AppBar>
+    </div>
 )
 
-const renderComponent = (user: User, logout: Logout) => (
-    <div>
-        <OrdersView/>
-        <div>Hello {user ? user.name : "guest"}</div>
-        <button onClick={() => logout({returnTo: window.location.origin})}>Log out</button>
+const renderComponent = (user: User, logout: Logout, classes: Styling) => (
+    <div className={classes.root}>
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon/>
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    Hello {user ? user.name : "guest"}
+                </Typography>
+                <Button color="inherit" onClick={() => logout({returnTo: window.location.origin})}>Log out</Button>
+            </Toolbar>
+        </AppBar>
+        <Paper>
+            <OrdersView/>
+        </Paper>
     </div>
 )
 
